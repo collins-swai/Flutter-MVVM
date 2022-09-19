853c22f87a5d944bf2963de2108c4dc7d1a14473
Collins Swai <kipkorirswai@gmail.com>
2022-09-19 17:09:43 +0300

    Widget loading for app

---
+++ lib/view/widget/LoadingWidget.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class LoadingWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircularProgressIndicator(),
+            SizedBox(
+              height: 8,
+            ),
+            Text(
+              "Loading...",
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
