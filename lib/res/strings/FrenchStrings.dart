bed25f20306f2ff11033eacf360e8e594d2eb55a
Collins Swai <kipkorirswai@gmail.com>
2022-09-19 17:05:10 +0300

    French strings for app

---
+++ lib/res/strings/FrenchStrings.dart
@@ -0,0 +1,15 @@
+import 'Strings.dart';
+
+class FrenchStrings extends Strings {
+  @override
+  String get homeScreen => "Décennie des films";
+
+  @override
+  String get movieDetailScreen => "Détails des films";
+
+  @override
+  String get titleCast => "Jeter";
+
+  @override
+  String get titleCategories => "Catégories";
+}
