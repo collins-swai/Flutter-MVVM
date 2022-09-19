6c2134e1f2acf2e7df313273930c8347ac20051c
Collins Swai <kipkorirswai@gmail.com>
2022-09-19 17:18:01 +0300

    Error loading for app

---
+++ lib/view/widget/MyErrorWidget.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+
+class MyErrorWidget extends StatelessWidget {
+  final String msg;
+
+  MyErrorWidget(this.msg);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        child: Text('$msg'),
+      ),
+    );
+  }
+}
